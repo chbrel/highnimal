bbe11f4134cf9936254c1fd09273777dd3f7077d
Loic Morvan <loic.morvan.fr@gmail.com>
2011-03-19 22:55:33 +0100

    Added parentanimals.php

---
+++ system/application/models/parentanimals.php
@@ -0,0 +1,44 @@
+<?php
+class ParentAnimals extends Model {
+
+    function ParentAnimals()
+    {
+        parent::Model();
+    }
+    
+    function get($id_parentanimal) {
+    	$this->db->from('parentanimals')->where('id = '.$id_parentanimal);
+    	$query = $this->db->get();
+    	
+    	$result = $query->result();
+    	
+    	return $result[0];
+    }
+    
+    function create($name, $species, $race, $birthdate, $sex, $pedigree) {    
+		$data = array(
+               'id' => '' ,
+               'species' => $species->id,
+			   'race' => $race,
+			   'birthdate' => $birthdate,
+			   'sex' => $sex,
+			   'pedigree' => $pedigree
+            );
+
+		$query = $this->db->insert('parentanimals', $data);
+		
+		$newId = $this->db->insert_id();
+				
+		return $newId;
+    }
+    
+    function update($data, $id_parentanimal) {
+    	$this->db->where('id = '.$id_parentanimal);
+    	$this->db->update('parentanimals', $data);
+    }
+    
+    function delete($id_parentanimal) {
+    	$this->db->where('id = '.$id_parentanimal);
+		$this->db->delete('parentanimals');
+    }
+}
