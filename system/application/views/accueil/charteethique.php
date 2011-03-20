ce1cb6298c9defc966a74c0150aa5c2ccbd550f9
Christian Brel <ch.brel@gmail.com>
2011-03-20 15:29:09 +0100

    css modif

---
+++ system/application/views/accueil/charteethique.php
@@ -0,0 +1,50 @@
+<div id="maincontent">
+<h1>Charte d’éthique</h1>
+
+Le site Highnimal souhaite développer dans cette Charte d'éthique les idées fortes qui animent ses membres et mettre en évidence la responsabilité de chacun et les moyens à utiliser pour les assumer. Cette Charte d'éthique est élaborée avec le souci de protéger et reconnaître les propriétaires d’animaux de pedigree, mais aussi pour leur offrir certaines garanties.
+
+<h2>Les parties</h2>
+<h3>La société Highnimal</h3>
+Highnimal est une place de marché de mise en relation de propriétaires d’animaux de pedigree pour les aider à trouver l’animal reproducteur idéal afin de garantir la qualité de la descendance, grâce à l’affichage de fiches détaillées selon des critères de sélection précis.
+<h3>Le propriétaire d’animal de pedigree</h3>
+Le propriétaire d’animal de pedigree est défini comme une personne possédant au moins un animal reproducteur de pedigree et souhaitant, par le biais de l’annuaire proposé par Highnimal, trouver un partenaire reproducteur de qualité et certifié. Le propriétaire peut être éleveur professionnel ou non, ou simple détenteur d’un animal reproducteur.
+
+<h2>Engagement de Highnimal</h2>
+Highnimal s’engage à :
+-	Veiller au bon respect des conditions d’utilisation de la plateforme de fiches
+-	Assurer un rôle de modérateur dans le contenu des fiches détaillées des propriétaires et, le cas échéant, lors de transaction entre deux propriétaires
+-	Garantir et respecter l’intégrité des propriétaires
+-	Respecter et s’assurer du respect des articles du Code Rural et de la Pêche Maritime visés par les activités exercées par le biais de la plateforme, ainsi que des organismes d’enregistrement des animaux de pedigree (voir document base réglementaire)
+-	Déclarer auprès de la CNIL les données enregistrées sur la plateforme et à respecter les conditions de partage et de confidentialité de ces données
+Highnimal se dégage de toute responsabilité en cas de :
+-	Publication de données mensongères dans les fiches détaillées des propriétaires d’animal de pedigree
+-	Naissance d’une portée non confirmable
+<h2>Engagement du propriétaire d’animal de pedigree</h2>
+Le propriétaire d’animal de pedigree s’engage à :
+-	Respecter les conditions d’utilisation de la plateforme et les données mises en ligne
+-	Etre majeur lors de son inscription sur la plateforme
+-	Etre détenteur des certificats d’immatriculation et de filiation des animaux enregistrés sur la plateforme
+-	Etre détenteur d'un affixe, dans le cas d’un éleveur
+-	Etre en possession du certificat de capacité à l’élevage, dans le cas d’un éleveur
+-	Etre en possession du certificat d’aptitude à la détention d’animaux dangereux le cas échéant
+-	Etre en possession du certificat de capacité d’élevage d’animaux non domestiques le cas échéant
+-	Justifier à l’aide d’attestation, la participation de son(ses) animal(aux) à tout événement de type concours, salon, exposition, foire, …
+-	Etre en possession des carnets de santé et certificats de vaccination des animaux
+-	Suivre régulièrement le protocole de vaccination et de vermifugation, selon l’espèce concernée
+-	Fournir tous les documents demandés lors de l’inscription
+-	Conserver une attitude correcte et courtoise envers les autres propriétaires
+-	Élever, entretenir, nourrir tous les chiens de l'élevage dans des conditions d'hygiène et de salubrité correcte. 
+-	Respecter les articles du Code Rural et de la Pêche Maritime visés par les activités exercées par le biais de la plateforme, ainsi que des organismes d’enregistrement des animaux de pedigree (voir document base réglementaire)
+-	Respecter les droits de l’animal
+-	Signaler toute cession d’animal sur la plateforme
+-	Respecter les fréquences et âge de saillie des femelles reproductrices
+-	Ne pas accepter de faire une saillie avec une femelle reproductrice en mauvais état sanitaire
+-	Prévenir la plateforme en cas de portée non confirmable
+-	Dans le cas d’une transaction, l’annonceur s’engage à accepter d’être évalué par le « répondeur » sur la qualité de l’élevage et de l’animal
+Le propriétaire d’animaux de pedigree se voit cependant libre de refuser toute transaction qui irait à l’encontre de cette charte d’éthique, de l’image de marque et de l’intégrité de son activité.
+
+
+<h2>Agrément de la présente charte par un organisme sanitaire et vétérinaire</h2>
+(à définir)
+
+</div>
