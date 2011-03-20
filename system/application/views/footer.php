b9e16669affe91c8ca6e89550497a3744996279b
Christian Brel <ch.brel@gmail.com>
2011-03-20 05:33:45 +0100

    Design integration

--- system/application/views/footer.php
+++ system/application/views/footer.php
@@ -5,35 +5,37 @@
 				Highnimal
 			</div>
 			<div class="sidebar_item_content">
-				Qualité, Simplicité, Ethique
+				    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
 			</div>
 		</div>
 		<div class="sidebar_item">
 			<div class="sidebar_item_title">
+				Qualité, Simplicité, Ethique
 			</div>
 			<div class="sidebar_item_content">
+				    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
 			</div>
 		</div>
 	</div>
 	<div class="clear"> </div>
 	<div id="footer" role="contentinfo">
 		<div id="footerinfos">
-			<div id="footerleft">
+			<div id="footerleft" class="footercolumn">
 				<div class="footer_item">Le Blog</div>
 				<div class="footer_item">Conseils Animaliers</div>
 			</div>
-			<div id="footercenter">
+			<div id="footercenter" class="footercolumn">
 				<div class="footer_item">Partenaires</div>
 				<div class="footer_item">Contact</div>
 				<div class="footer_item">Aide</div>
 			</div>
-			<div id="footerright">
+			<div id="footerright" class="footercolumn">
 				<div class="footer_item">Conditions d'utilisations</div>
 				<div class="footer_item">Informations Légales</div>
 				<div class="footer_item">Charte Ethique</div>
 			</div>
 		</div>
-		
+		<div class="clear"> </div>
 		<div id="highnimalright">
 			<div id="copyright">Copyright © highnimal.com</div>
 			<div id="designby">Design by <?=anchor('http://pixanimal.com', 'Pixanimal', array('title' => 'Pixanimal')); ?> -- <?=anchor('http://c.line-design.fr', 'C.line', array('title' => 'C.line')); ?></div>
